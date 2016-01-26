f68494df5bbe8212bb5b3d506d9a62cac00d9fd3
AndreikoVan <andreikovan@outlook.com>
2016-01-26 15:47:22 +0200

    ddd

---
+++ js/main.js
@@ -0,0 +1,24 @@
+﻿var strLog = "";
+function dump() {
+    if (strLog == "") {
+        alert("you are not setting up dump information");
+    }
+    else {
+        console.log(strLog);
+    }
+}
+
+function leftbutton_click() {
+    strLog = "you are clicked left"; dump();
+    var $logoImage = $("#logo.img");
+    $logoImage.css("left", "120");
+
+
+}
+function rightbutton_click() {
+    strLog = "you are clicked right"; dump();
+}
+
+function logo_click() {
+    strLog = "you are clicked logo"; dump();
+}
